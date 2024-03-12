b79ff1436f7d0472d0a1113e0791ac650e6a3d4d
Pablo <pablonumon@gmail.com>
2024-03-12 12:23:51 +0100

    cambios

--- app/src/main/java/com/example/bibliotecatfg/ActivityRegistro.java
+++ app/src/main/java/com/example/bibliotecatfg/ActivityRegistro.java
@@ -1,7 +1,15 @@
 package com.example.bibliotecatfg;
 
+
+import android.os.Bundle;
+
+
 import androidx.appcompat.app.AppCompatActivity;
 
 public class ActivityRegistro extends AppCompatActivity {
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_registro);
 
+    }
 }
